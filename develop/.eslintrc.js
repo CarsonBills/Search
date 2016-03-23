516c91df468fcbcb2103a07deee9d7f63bf13a03
Hon Chih Chen <hchen@wwnorton.com>
2016-03-23 09:43:47 -0400

    initial project setup fileinclude

---
+++ develop/.eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "strict": [
+            2,
+            "function"
+        ],
+        "indent": [
+            2,
+            "tab"
+        ],
+        "linebreak-style": [
+            2,
+            "unix"
+        ],
+        "quotes": [
+            2,
+            "single"
+        ],
+        "semi": [
+            2,
+            "always"
+        ]
+    }
+};
---
+++ develop/gulpfile.js
@@ -0,0 +1,103 @@
+var gulp = require('gulp'),
+    $ = require("gulp-load-plugins")({
+            pattern: ['gulp-*', 'gulp.*'],
+            replaceString: /\bgulp[\-.]/
+        }),
+    del         = require('del'),
+    gulpif      = require('gulp-if'),
+    hbsfy       = require('browserify-handlebars'),
+    fileinclude = require('gulp-file-include'),
+    htmlreplace = require('gulp-html-replace'),
+    svgSprite = require('gulp-svg-sprite'),
+
+    app = "app/",
+    deploy = "deploy/",
+    settings = {
+        page_templates: "page_templates/", // file-include templates
+        templates: "templates/", // handlebars templates
+        js: "js/",
+        json: "json/",
+        fonts: "fonts/",
+        sass: "sass/",
+        sass_sprite: '/sass/sprite/',
+        images: "images/",
+        svg_sprite: '/images/svg_sprite.svg',
+        svg: "images/svg/",
+        js_vendor: "js/vendor/",
+        css: "css/"
+    };
+
+function getHTMLAssets(path) {
+    return {
+        css: {
+            src: path +'css/style.css',
+            tpl: '<link rel="stylesheet" href="%s">'
+        },
+        js: {
+            src: [
+                "js/vendor/jwplayer.js",
+                'js/bundle.min.js'
+            ],
+            tpl: '<script src="%s"></script>'
+        }/*,
+        icon: {
+            src: [path + '/images/favicon.png'],
+            tpl: '<link rel="icon" type="image/png" href="%s">'
+        }*/
+    }
+}
+
+var VERSION = (process.env.VERSION) ? process.env.VERSION : "dev"
+
+function isDev() {
+    return VERSION === 'dev';
+}
+
+gulp.task('fileinclude', function () {
+    var assets = getHTMLAssets('');
+    return gulp.src([
+            app + settings.page_templates + '*.html'
+        ])
+        .pipe($.plumber(function (error) {
+            $.util.beep();
+            $.util.log($.util.colors.red(error));
+            this.emit('end');
+        }))
+        .pipe(fileinclude({
+            prefix: '@@',
+            basepath: '@file'
+        }))
+        //.pipe(htmlreplace(assets))
+        .pipe(gulp.dest(deploy))
+        .pipe($.size())
+        .pipe($.notify({
+            message: ' fileinclude done'
+        }))
+        .pipe(gulpif(isDev(), $.livereload()));
+});
+
+
+gulp.task('eslint', function() {
+	return gulp.src(settings.js + "**/*.js")
+  		.pipe($.eslint({
+  		}))
+  		.pipe($.eslint.format());
+  		// Brick on failure to be super strict
+  		//.pipe($.eslint.failOnError());
+});
+
+
+gulp.task('clean', del.bind(null, [deploy + '*']));
+
+gulp.task('watch', ['fileinclude'], function (e) {
+
+    $.livereload.listen();
+    $.connect.server({
+        root: deploy,
+        port:7000,
+        livereload: false
+    });
+
+    gulp.watch([app + settings.page_templates + '**/*.html'], ['fileinclude']);
+    //gulp.watch([app + settings.js + "**/*.js"], ['eslint']);
+});
